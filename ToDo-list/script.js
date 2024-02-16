5a05fd9b963710f7338378e6082ff3efc9856132
ChoiGYoung <choidh1261@naver.com>
2024-02-16 18:24:00 +0900

    구조분해할당

--- ToDo-list/script.js
+++ ToDo-list/script.js
@@ -1,48 +1,48 @@
-const todoInput = document.querySelector('#todo-input');
-const todoList = document.querySelector('#todo-list');
+const todoInput = document.querySelector("#todo-input");
+const todoList = document.querySelector("#todo-list");
 
-const savedTodoList = JSON.parse(localStorage.getItem('saved-items'));
+const savedTodoList = JSON.parse(localStorage.getItem("saved-items"));
 
 const createTodo = function (storageData) {
-    let todoContents = todoInput.value;
-    if (storageData) {
-      todoContents = storageData.contents;
-    }
-    
-    const newLi = document.createElement('li');
-    const newSpan = document.createElement('span');
-    const newBtn = document.createElement('button');
-  
-    newBtn.addEventListener('click', () => {
-      newLi.classList.toggle('complete');
-      saveItemsFn();
-    });
-  
-    newLi.addEventListener('dblclick', () => {
-      newLi.remove();
-      saveItemsFn();
-    });
-  
-    if (storageData && storageData.complete) {
-      newLi.classList.add('complete');
-    }
-  
-    newSpan.textContent = todoContents;
-    newLi.appendChild(newBtn);
-    newLi.appendChild(newSpan);
-    todoList.appendChild(newLi);
-    todoInput.value = '';
+  let todoContents = todoInput.value;
+  if (storageData) {
+    todoContents = storageData.contents;
+  }
+
+  const newLi = document.createElement("li");
+  const newSpan = document.createElement("span");
+  const newBtn = document.createElement("button");
+
+  newBtn.addEventListener("click", () => {
+    newLi.classList.toggle("complete");
     saveItemsFn();
-  };
+  });
+
+  newLi.addEventListener("dblclick", () => {
+    newLi.remove();
+    saveItemsFn();
+  });
+
+  if (storageData && storageData.complete) {
+    newLi.classList.add("complete");
+  }
+
+  newSpan.textContent = todoContents;
+  newLi.appendChild(newBtn);
+  newLi.appendChild(newSpan);
+  todoList.appendChild(newLi);
+  todoInput.value = "";
+  saveItemsFn();
+};
 
 const keyCodeCheck = function () {
-  if (window.event.keyCode === 13 && todoInput.value.trim() !== '') {
+  if (window.event.keyCode === 13 && todoInput.value.trim() !== "") {
     createTodo();
   }
 };
 
 const deleteAll = function () {
-  const liList = document.querySelectorAll('li');
+  const liList = document.querySelectorAll("li");
   for (let i = 0; i < liList.length; i++) {
     liList[i].remove();
   }
@@ -53,15 +53,15 @@ const saveItemsFn = function () {
   const saveItems = [];
   for (let i = 0; i < todoList.children.length; i++) {
     const todoObj = {
-      contents: todoList.children[i].querySelector('span').textContent,
-      complete: todoList.children[i].classList.contains('complete'),
+      contents: todoList.children[i].querySelector("span").textContent,
+      complete: todoList.children[i].classList.contains("complete"),
     };
     saveItems.push(todoObj);
   }
 
   saveItems.length === 0
-    ? localStorage.removeItem('saved-items')
-    : localStorage.setItem('saved-items', JSON.stringify(saveItems));
+    ? localStorage.removeItem("saved-items")
+    : localStorage.setItem("saved-items", JSON.stringify(saveItems));
 };
 
 if (savedTodoList) {
@@ -70,30 +70,34 @@ if (savedTodoList) {
   }
 }
 
-const weatherSearch = function (position) {
-     const openWeatherRes = fetch(
-        `https://api.openweathermap.org/data/2.5/weather?lat=${position.latitude}&lon=${position.longitude}&appid=f9ea4b92f947d4e9502de99f890e7b70`
-        ).then((res) => {
-          return res.json();
-        }).then((json) => {
-          console.log(json.name, json.weather[0].description);
-        }).catch((err) => {
-          console.log(err);
-        })
-    }
+const weatherSearch = function ({latitude, longitude}) {
+  const openWeatherRes = fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=f9ea4b92f947d4e9502de99f890e7b70`
+  )
+    .then((res) => {
+      return res.json();
+    })
+    .then((json) => {
+      console.log(json.name, json.weather[0].main);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
-const accessToGeo = function (position) {
-    const positionObj = {
-        latitude: position.coords.latitude,
-        longitude : position.coords.longitude,
-    }
-    weatherSearch(positionObj)
-}
+const accessToGeo = function ({ coords }) {
+  const {latitude, longitude} = coords
+  // shorthand property
+  const positionObj = {
+    latitude,
+    longitude,
+  };
+  weatherSearch(positionObj);
+};
 
 const askForLocation = function () {
-    navigator.geolocation.getCurrentPosition(accessToGeo, (err) => {
-        console.log(err);
-    })
-}
+  navigator.geolocation.getCurrentPosition(accessToGeo, (err) => {
+    console.log(err);
+  });
+};
 askForLocation();
-
